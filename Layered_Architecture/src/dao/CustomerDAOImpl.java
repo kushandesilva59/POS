89a00146453e16665dab2b8f2feec5aaadec276c
kushandesilva59 <kushandesilva801@gmail.com>
2022-04-28 10:30:17 +0530

    POS System added

---
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -0,0 +1,46 @@
+package dao;
+
+import db.DBConnection;
+import model.CustomerDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl {
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+
+        ArrayList<CustomerDTO> allCustomers = new ArrayList<>();
+        while (rst.next()){
+            allCustomers.add(new CustomerDTO(rst.getString(1),rst.getString(2),rst.getString(3)));
+        }
+        return allCustomers;
+    }
+
+    public boolean saveCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1, dto.getId());
+        pstm.setString(2, dto.getName());
+        pstm.setString(3, dto.getAddress());
+        return pstm.executeUpdate() > 0;
+    }
+
+    public boolean updateCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+        pstm.setString(1, dto.getName());
+        pstm.setString(2, dto.getAddress());
+        pstm.setString(3, dto.getId());
+        return pstm.executeUpdate()>0;
+    }
+
+    public boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
+        pstm.setString(1, id);
+        return pstm.executeUpdate()>0;
+    }
+}
